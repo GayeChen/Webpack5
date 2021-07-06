96d032b3a3fc4e5027fecb2b04862babe890f2e0
chengxiao01 <chengxiao01@baidu.com>
2021-07-07 00:11:03 +0800

    first commit

---
+++ packages/base/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDom from 'react-dom';
+import png from './assets/logo.png'
+import jpg from './assets/logo.jpg'
+import txt from './assets/logo.txt'
+import ico from './assets/logo.ico'
+import avator from './assets/avator.jpg';
+
+console.log('png:', png, avator);
+console.log('jpg:', jpg);
+console.log('ico:', ico);
+console.log('txt:', txt);
+
+ReactDom.render(
+  <h1>hello4</h1>,
+  document.getElementById('root')
+);
---
+++ packages/base/webpack.config.js
@@ -0,0 +1,60 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  devtool: false,
+  entry: './src/index.js',
+  cache: {
+    type: 'memory'
+  },
+  // watch: true,
+  devServer: {
+    port: 8080
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                '@babel/preset-env',
+                '@babel/preset-react'
+              ]
+            }
+          }
+        ]
+      },
+      {
+        test: /\.png$/,
+        type: 'asset/resource' // 相当于file-loader，输出文件到目录
+      },
+      {
+        test: /\.ico$/,
+        type: 'asset/inline' // 相当与url-loader， 处理成base64
+      },
+      {
+        test: /\.txt/,
+        type: 'asset/source' // 相当与raw-loader，将文件处理成字符串导入
+      },
+      {
+        test: /\.jpg$/,
+        type: 'asset',
+        parser: {
+          dataUrlCondition: {
+            maxSize: 4 * 1024 // 超过此阈值 使用asset/resource，小于则使用inline
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html'
+    })
+  ]
+}
